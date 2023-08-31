5cb7fb652348917aceeda893d668efa526e4df18
kiranbachhav897 <kiranbachhav897@gmail.com>
2023-08-31 23:17:25 +0530

    Practical1 Uploaded!!

---
+++ Assign_1.py
@@ -0,0 +1,33 @@
+#Tokens in spacy
+import spacy
+nlp = spacy.load("en_core_web_sm")
+about_text = (
+    "Gus Proto is a Python developer currently"
+    " working for a London-based Fintech"
+    " company. He is interested in learning"
+    " Natural Language Processing."
+)
+about_doc = nlp(about_text)
+
+for token in about_doc:
+    print (token, token.idx)
+
+#stop words dispay
+spacy_stopwords = spacy.lang.en.stop_words.STOP_WORDS
+len(spacy_stopwords)
+
+for stop_word in list(spacy_stopwords)[:10]:
+    print(stop_word)
+
+#stop words removal 
+custom_about_text = (
+    "Gus Proto is a Python developer currently"
+    " working for a London-based Fintech"
+    " company. He is interested in learning"
+    " Natural Language Processing."
+)
+nlp = spacy.load("en_core_web_sm")
+about_doc = nlp(custom_about_text)
+print([token for token in about_doc if not token.is_stop])
+
+
